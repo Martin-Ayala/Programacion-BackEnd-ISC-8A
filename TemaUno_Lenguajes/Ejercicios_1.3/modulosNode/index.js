2bcdec6070528c2b8ed7632e6b8fd019c68a5222
Martin <martinadrianayala@gmail.com>
2026-02-24 16:27:17 -0500

    Ejercicios Node

--- TemaUno_Lenguajes/Ejercicios_1.3/modulosNode/index.js
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNode/index.js
@@ -3,4 +3,13 @@ const fs = require('fs'); // Importa la herramienta de archivos
 fs.writeFile('archivo.txt', 'Hola desde Node.js', (err) => {
     if (err) throw err; // Si hay un error, lo lanza
     console.log('El archivo ha sido creado con exito');
+
+    // DESAFIO 1
+    fs.readFile('archivo.txt', 'utf8', (err, data) => { //utf8 para que el contenido se muestre como texto
+    if (err) throw err;
+    console.log('Contenido del archivo:', data); // Muestra el contenido del archivo
+    });
+
+
 });
+
